2bded831bf703e530ef9979f5422f77f6acda690
John Chen <jglchen@gmail.com>
2020-11-18 19:32:31 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    characters: [],
+    characterEdit: null
+  };  
+  
+  removeCharacter = (index) => {
+     const {characters} = this.state
+
+     this.setState({
+        characters: characters.filter((character) => {
+           return character.id !== index
+        }),
+     })
+  };  
+  
+  editCharacter = (index) => {
+     const {characters} = this.state
+     let character = characters.find(item => item.id == index);
+     this.setState({characterEdit: character});
+  };  
+  
+  handleSubmit = (character) => {
+      if (character.id === -1){
+         let id = this.state.characters.length ? this.state.characters[this.state.characters.length-1].id + 1 : 0;
+         character.id = id;
+         this.setState({characters: [...this.state.characters, character]});
+         return;
+      }
+      
+      let characters = this.state.characters;
+      let idx = characters.findIndex(item => item.id == character.id);
+      characters[idx] = character;
+      this.setState({characters: characters, characterEdit: null});
+  }; 
+  
+  render() {
+    const { characters, characterEdit } = this.state;
+    let headTitle = 'Add New';
+    let descriptionTitle = 'Add a character with a name and a job to the table.';
+    if (characterEdit){
+       headTitle = 'Update Record';
+       descriptionTitle = "Update the selected character's record";
+    }
+    
+    return (
+      <div className="container">
+        <h1>React App Demo</h1>
+        <p>{descriptionTitle}</p>        
+        <Table 
+             characterData={characters} 
+             removeCharacter={this.removeCharacter} 
+             editCharacter={this.editCharacter} 
+             characterEdit={characterEdit}
+        />
+        <h3>{headTitle}</h3>
+        <Form characterEdit={characterEdit} handleSubmit={this.handleSubmit} />
+      </div>
+    )
+  }
 }
 
 export default App;
